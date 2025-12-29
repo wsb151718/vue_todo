b028d1c97c69754a30fcc4edce9edb1d8de1d0e0
wsb151718 <satoshiyymok@gmail.com>
2025-12-29 22:36:04 +0900

    2. Todo の状態管理 完了

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,11 +2,19 @@
 import TodoItem from './TodoItem.vue'
 
 const props = defineProps(['list'])
+const emits = defineEmits(['toggle-finished'])
 </script>
 
 <template>
   <ul class="p-todoList">
-    <TodoItem v-for="item in list" :key="item.id" :text="item.text"></TodoItem>
+    <TodoItem
+      v-for="item in list"
+      :key="item.id"
+      :text="item.text"
+      :id="item.id"
+      :finished="item.finished"
+      @change-finished="$emit('toggle-finished', item)"
+    ></TodoItem>
   </ul>
 </template>
 
