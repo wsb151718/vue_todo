e5b297eaef86c2ac1a2136396bbcf11ac9f01835
satoshi tsubokura <tsubokurajob151718@gmail.com>
2025-12-29 14:51:48 +0900

    見た目のみ作成

---
+++ src/components/FormInput.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(['placeholder'])
+</script>
+
+<template>
+  <input type="text" class="c-input" :placeholder="placeholder" />
+</template>
+
+<style scoped>
+.c-input {
+  background-color: #eff3f5;
+  border-radius: 8px;
+  border: 1px solid #aaa;
+  font-size: 1.4rem;
+  padding: 6px;
+
+  &:placeholder-shown {
+    color: #aaa;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Todo from './Todo.vue'
 
-createApp(App).mount('#app')
+createApp(Todo).mount('#app')
