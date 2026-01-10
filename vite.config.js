089f8d741378f56dfa0444c90b284efc44ca5c42
wsb151718 <satoshiyymok@gmail.com>
2026-01-10 23:24:32 +0900

    コンポーネントテストライブラリ追加

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { playwright } from '@vitest/browser-playwright'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,8 +14,22 @@ export default defineConfig({
     },
   },
   test: {
-    include: ['**/*.{test,spec}.{ts,tsx,js,jsx}'],
     exclude: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/.output/**', '**/coverage/**'],
+    include: ['**/*.{test,spec}.{ts,tsx,js,jsx}'],
     environment: 'happy-dom',
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'browser',
+          include: ['test/components/**/*.{test,spec}.{ts,tsx,js,jsx}'],
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [{ browser: 'chromium' }],
+          },
+        },
+      },
+    ],
   },
 })
