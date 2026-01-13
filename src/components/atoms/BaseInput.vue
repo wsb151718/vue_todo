7ec114d62ec3772b491a2e71803cf1fdd783c7c2
wsb151718 <satoshiyymok@gmail.com>
2026-01-13 22:35:39 +0900

    input のlazy対応

--- src/components/atoms/BaseInput.vue
+++ src/components/atoms/BaseInput.vue
@@ -1,15 +1,32 @@
 <script setup>
-import { useTemplateRef } from 'vue'
+import { useTemplateRef, ref, watch } from 'vue'
 
 defineProps({
   placeholder: { type: String, default: '' },
-  isFocus: Boolean,
 })
-const [inputModel] = defineModel({ type: String })
+const [inputModel, modifired] = defineModel({ type: String })
+const inputValue = ref(inputModel.value ?? '')
 const inputComponent = useTemplateRef('input-text')
 function focus() {
   inputComponent.value.focus()
 }
+
+function inputHandler(event) {
+  inputValue.value = event.target.value
+  if (!modifired.lazy) {
+    inputModel.value = inputValue.value
+  }
+}
+
+function changeHandler() {
+  if (modifired.lazy) {
+    inputModel.value = inputValue.value
+  }
+}
+
+watch(inputModel, (value) => {
+  inputValue.value = value ?? ''
+})
 
 defineExpose({ focus })
 </script>
@@ -17,10 +34,12 @@ defineExpose({ focus })
 <template>
   <input
     ref="input-text"
-    v-model="inputModel"
+    :value="inputValue"
     type="text"
     class="c-input"
     :placeholder="placeholder"
+    @input="inputHandler"
+    @change="changeHandler"
   />
 </template>
 
@@ -31,6 +50,7 @@ defineExpose({ focus })
   border: 1px solid #aaa;
   font-size: 1.4rem;
   padding: 6px;
+  width: 100%;
 
   &:placeholder-shown {
     color: #aaa;
