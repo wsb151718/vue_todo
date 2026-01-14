529d1e8f3e434e97e410673290e4485045dc67fc
wsb151718 <satoshiyymok@gmail.com>
2026-01-14 22:36:26 +0900

    モーダルコンポーネント作成

---
+++ src/components/atoms/BaseModal.vue
@@ -0,0 +1,59 @@
+<script setup>
+defineOptions({
+  inheritAttrs: false,
+})
+
+const isOpen = defineModel({
+  type: Boolean,
+})
+
+function close() {
+  isOpen.value = false
+}
+</script>
+
+<template>
+  <div class="c-modal js-modal">
+    <div v-click-outside="close" class="c-modal__container" v-bind="$attrs">
+      <a class="c-modal__close" href="javascript:void(0)" @click="close">×</a>
+      <div class="c-modal__main">
+        <slot name="header"></slot>
+        <slot></slot>
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.c-modal {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  z-index: 5000;
+  background: rgba(0, 0, 0, 0.5);
+}
+.c-modal__container {
+  width: 600px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-height: 80vh;
+  background-color: #fff;
+  border-radius: 16px;
+  padding: 30px;
+  z-index: 1000;
+}
+.c-modal__close {
+  display: block;
+  font-size: 4rem;
+  line-height: 1;
+  text-align: right;
+  margin-bottom: 20px;
+}
+.c-modal__main {
+  padding-inline: 20px;
+}
+</style>
---
+++ src/components/molecules/TodoModal.vue
@@ -0,0 +1,126 @@
+<script setup>
+import BaseModal from '../atoms/BaseModal.vue'
+import BaseButton from '../atoms/BaseButton.vue'
+import BaseInput from '../atoms/BaseInput.vue'
+import ErrorMsg from '../atoms/ErrorMsg.vue'
+import { ref } from 'process'
+import { useValidator } from '@/plugins/validate'
+
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+})
+
+const emits = defineEmits({
+  deleteItem: null,
+  editItem: (text) => {
+    if (!text && typeof text !== 'string') {
+      return false
+    }
+
+    return true
+  },
+})
+
+const input = ref(props.text.value)
+
+const validate = useValidator()
+const error = ref({})
+function submitHandler() {
+  error.value = {}
+  // Todo TodoItemと同じバリデーションなので、バリデーションルールをどこかに一元化する。
+  error.value.title =
+    validate({ value: input.value, text: 'タスク名' }, { required: true, maxLength: 100 })[0] ?? ''
+  if (!error.value) {
+    input.value = input.value.trim()
+    emits('editItem', input.value)
+  }
+}
+</script>
+
+<template>
+  <Transition name="fade">
+    <BaseModal v-if="isModalOpen" v-model="isModalOpen">
+      <template #header>
+        <div class="c-modal__heading">
+          <h3 class="c-modal__title">{{ input }}</h3>
+          <BaseButton is-alert @click="$emit('deleteItem')">削除</BaseButton>
+        </div>
+      </template>
+      <div class="c-modal__main">
+        <form class="c-form" action="" @submit.prevent="submitHandler">
+          <dl class="c-form-row">
+            <dt>
+              <label for="modal-todo-task">タスク名: </label>
+            </dt>
+            <dd>
+              <BaseInput id="modal-todo-task" v-model.lazy="input" />
+              <ErrorMsg>
+                {{ error.title }}
+              </ErrorMsg>
+            </dd>
+          </dl>
+          <div class="c-form__buttons">
+            <BaseButton class="c-form__button--edit" type="submit">編集</BaseButton>
+          </div>
+        </form>
+      </div>
+    </BaseModal>
+  </Transition>
+</template>
+
+<style scoped>
+.c-modal__heading {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+.c-modal__title {
+  font-size: 2.4rem;
+}
+.c-modal__main {
+}
+.c-form-row {
+  --gap: 20px;
+  --labelW: 100px;
+  width: 80%;
+  display: flex;
+  align-items: center;
+  flex-flow: wrap;
+  gap: var(--gap);
+  margin-bottom: 20px;
+
+  & dt {
+    flex: 0 var(--labelW);
+    font-size: 1.4rem;
+  }
+  & dd {
+    flex: 1 calc(100% - (var(--labelW) + var(--gap)));
+  }
+}
+.c-form__buttons {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+.c-form__button--edit {
+  width: 60px;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+</style>
